adfeb49c756758df762762a4e1d8bbe3933b5c4d
Maksim <maksimshchavelev@gmail.com>
2025-07-06 16:51:53 +0300

    refactor, server: code style refactor

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,39 +1,31 @@
-#include <iostream>
-#include <format>
 #include "version.hpp"
 #include <cxxopts.hpp>
+#include <format>
+#include <iostream>
 
-
-int main(int argc, char** argv)
-{
+int main(int argc, char **argv) {
     cxxopts::Options options("smu-server", "Server part included in server-monitoring-utility");
-    options.add_options()
-        ("version", "Show smu-server version")
-        ("h,help", "Show help information");
+
+    options.add_options()("version", "Show smu-server version")("h,help", "Show help information");
 
     cxxopts::ParseResult result;
 
-    try
-    {
+    try {
         result = options.parse(argc, argv);
-    } catch (std::exception& e)
-    {
+    } catch (std::exception &e) {
         std::cout << "Argument parsing error: " << e.what() << std::endl;
         return EXIT_FAILURE;
     }
 
-
-    if(result.contains("version"))
-    {
+    if (result.contains("version")) {
         std::cout << std::format("smu-server version is {}.{}.{}",
                                  PROJECT_VERSION_MAJOR,
                                  PROJECT_VERSION_MINOR,
-                                 PROJECT_VERSION_PATCH) << std::endl;
-    } else if(result.contains("help") || result.contains("h"))
-    {
+                                 PROJECT_VERSION_PATCH)
+                  << std::endl;
+    } else if (result.contains("help") || result.contains("h")) {
         std::cout << options.help();
     }
-
 
     return EXIT_SUCCESS;
 }
