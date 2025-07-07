d8f03e69fc0026cba118daa98988826c83ff1cac
Maksim <maksimshchavelev@gmail.com>
2025-07-07 13:38:52 +0300

    feat: add MainWebsocketController

---
+++ server/src/controllers/websocket_main_controller.cpp
@@ -0,0 +1,51 @@
+/// GPLv3 LICENSE, Copyright (©) 2025, Maksim Shchavelev <maksimshchavelev@gmail.com>
+/// See LICENSE for details
+
+/**
+ * @file core/controllers/websocket_main_controller.cpp
+ * @brief Main websocket controller implementation
+ */
+
+
+#include "core/controllers/websocket_main_controller.hpp"
+
+
+namespace smu_server {
+
+
+// Public method
+void MainWebsocketController::handleNewMessage(const drogon::WebSocketConnectionPtr&,
+                                               std::string&&,
+                                               const drogon::WebSocketMessageType&) {}
+
+
+
+
+// Public method
+void MainWebsocketController::handleNewConnection(
+    const drogon::HttpRequestPtr&, const drogon::WebSocketConnectionPtr& connection) {
+    m_connections.insert(connection);
+}
+
+
+
+
+// Public method
+void MainWebsocketController::handleConnectionClosed(
+    const drogon::WebSocketConnectionPtr& connection) {
+    m_connections.erase(connection);
+}
+
+
+
+
+// Public method
+void MainWebsocketController::send_everyone(const Json::Value& data) {
+    for (auto& connection : m_connections) {
+        connection->sendJson(data);
+    }
+}
+
+
+
+} // namespace smu_server
