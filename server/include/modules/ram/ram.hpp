e6e37d692e06864775983df87b7ec824b581a3f7
Maksim <maksimshchavelev@gmail.com>
2025-07-07 10:37:27 +0300

    feat: add blank for RAM module

---
+++ server/include/modules/ram/ram.hpp
@@ -0,0 +1,25 @@
+/// GPLv3 LICENSE, Copyright (©) 2025, Maksim Shchavelev <maksimshchavelev@gmail.com>
+/// See LICENSE for details
+
+/**
+ * @file modules/ram/ram.hpp
+ * @brief File with RAM module class
+ */
+
+#pragma once
+
+#include "core/core.hpp"
+
+namespace smu_server {
+
+
+/**
+ * @brief A module that allows you to get information about RAM
+ */
+class RAM : public IModule {
+public:
+    REGISTER_MODULE(RAM, "A module that allows you to get information about RAM");
+};
+
+
+} // end of namespace smu_server
