5966369ca8fef286ff1226e1a469dd6957b2d888
Maksim <maksimshchavelev@gmail.com>
2025-07-10 09:29:44 +0300

    feat: add ConfigManager to manage configs
    Configurations allow you to customize both the server and individual modules

---
+++ server/include/config_manager/config_manager.hpp
@@ -0,0 +1,83 @@
+/// GPLv3 LICENSE, Copyright (©) 2025, Maksim Shchavelev <maksimshchavelev@gmail.com>
+/// See LICENSE for details
+
+/**
+ * @file config_manager/config_manager.hpp
+ * @brief Global singleton configuration manager for application and module
+ * configuration management
+ */
+
+#pragma once
+
+#include <expected>
+#include <json/json.h>
+
+namespace smu_server {
+
+/**
+ * @brief Global singleton configuration manager for application and module
+ * configuration management
+ */
+class ConfigManager {
+  public:
+    /**
+     * @brief Get instance of ConfigManager
+     * @return Lvalue reference to `ConfigManager` instance
+     */
+    static ConfigManager& instance();
+
+
+
+
+    /**
+     * @brief Get *reference* to Json representation of server config
+     * @return *Lvalue reference* to `Json::Value` with server config
+     * @note Reads the configuration file on the first call
+     * @throw `std::runtime` config when error
+     */
+    Json::Value& get_server_config();
+
+
+
+
+    /**
+     * @brief Saves server config.
+     * @return `std::expected` with void if success, otherwise `std::string` with error
+     */
+    std::expected<void, std::string> save_server_config() const noexcept;
+
+
+
+
+  private:
+    ConfigManager() = default;
+    ~ConfigManager() = default;
+
+
+
+
+    /**
+     * @brief Reads config from file
+     * @param path Path to file with config
+     * @return `std::expected` with `Json::Value` if success and `std::string` if error
+     * @private
+     */
+    std::expected<Json::Value, std::string> read_config(std::string_view path) const noexcept;
+
+
+
+
+    /**
+     * @brief Saves config to file
+     * @param path Path to file with config
+     * @param config Json config
+     * @return `std::expected` with void if success, otherwise `std::string` with error
+     */
+    std::expected<void, std::string> save_config(std::string_view   path,
+                                                 const Json::Value& config) const noexcept;
+
+
+    Json::Value m_server_config{};
+};
+
+} // namespace smu_server
