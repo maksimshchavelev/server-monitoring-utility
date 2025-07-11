6a408278fc38bda8babb5784774c176a55c63966
Maksim <maksimshchavelev@gmail.com>
2025-07-11 16:02:09 +0300

    feat: add Application module

---
+++ client/include/modules/application/application.hpp
@@ -0,0 +1,79 @@
+/// GPLv3 LICENSE, Copyright (©) 2025, Maksim Shchavelev <maksimshchavelev@gmail.com>
+/// See LICENSE for details
+
+/**
+ * @file modules/application/application.hpp
+ * @brief File with application class to manage other modules
+ */
+
+#pragma once
+
+#include "modules/network/network.hpp"
+#include "modules/settings/settings.hpp"
+#include <atomic>
+#include <json/json.h>
+
+namespace smu {
+
+/**
+ * @brief The Application to manage other modules
+ */
+class Application {
+  public:
+    /**
+     * @brief Application constructor
+     * @param settings Reference to objcet of `smu::Settings` class
+     */
+    Application(Settings& settings);
+
+
+
+
+    /**
+     * @brief Runs the application and block the main thread
+     * @return `0` if success exiting, other error code if error
+     * @example
+     *
+     * @code{.cpp}
+     *
+     * int main() {
+     *  // Creating smu::Settings here
+     *
+     *  smu::Application app(settings);
+     *  return app.run();
+     * }
+     *
+     * @endcode
+     */
+    int run();
+
+
+
+
+    /**
+     * @brief Exit application
+     */
+    void exit();
+
+
+
+  private:
+    Settings&               m_settings;
+    Network                 m_network;
+    std::atomic_int         m_return_value{0};
+    std::atomic_bool        m_exit_request{false};
+    std::condition_variable cw; // for m_exit_request
+
+
+
+
+    /**
+     * @brief Converts string representation of json to `Json::Value`
+     * @param str String representation of json
+     * @return `std::expected` with `Json::Value` if success and `std::string` if error
+     * @private
+     */
+    std::expected<Json::Value, std::string> json_from_string(const std::string& str) const noexcept;
+};
+
+} // namespace smu
---
+++ client/src/modules/application/application.cpp
@@ -0,0 +1,71 @@
+/// GPLv3 LICENSE, Copyright (©) 2025, Maksim Shchavelev <maksimshchavelev@gmail.com>
+/// See LICENSE for details
+
+/**
+ * @file modules/application/application.cpp
+ * @brief File with application class to manage other modules
+ */
+
+#include "modules/application/application.hpp"
+#include <iostream>
+
+namespace smu {
+
+// Public constructor
+Application::Application(Settings& settings) : m_settings(settings), m_network(m_settings) {}
+
+
+
+
+// Public method
+int Application::run() {
+    m_network.run([this](const std::string& msg) {
+        if (auto res = json_from_string(msg); res.has_value()) {
+            // If no error
+            std::cout << res.value();
+        }
+    });
+
+    // Waiting for exit signal
+    std::mutex                   mutex;
+    std::unique_lock<std::mutex> lock(mutex);
+    cw.wait(lock, [this]() { return m_exit_request.load(); });
+
+    m_network.stop();
+
+    return m_return_value.load();
+}
+
+
+
+
+// Public method
+void Application::exit() {
+    m_exit_request.store(true);
+    cw.notify_one();
+}
+
+
+
+
+// Private method
+std::expected<Json::Value, std::string> Application::json_from_string(
+    const std::string& str) const noexcept {
+    const auto     raw_json_length = static_cast<int>(str.length());
+    JSONCPP_STRING err;
+    Json::Value    json;
+
+    Json::CharReaderBuilder                 builder;
+    const std::unique_ptr<Json::CharReader> reader(builder.newCharReader());
+    if (!reader->parse(str.c_str(), str.c_str() + raw_json_length, &json, &err)) {
+        // error
+        return std::unexpected(std::format("Error conversion to json: {}", err));
+    }
+
+    return json;
+}
+
+
+
+
+} // namespace smu
