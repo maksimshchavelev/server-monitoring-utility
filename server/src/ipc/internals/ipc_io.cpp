5a66168ace05b1cc93dc9d496b3e78e896d035e4
Maksim <maksimshchavelev@gmail.com>
2025-07-18 16:23:40 +0300

    refactor: reduce scope of variable rd_res because of cppcheck

--- server/src/ipc/internals/ipc_io.cpp
+++ server/src/ipc/internals/ipc_io.cpp
@@ -181,10 +181,8 @@ std::expected<std::string, std::string> IPC_IO::receive_message(int        conne
     std::string result;
     char        buffer[256];
 
-    ssize_t rd_res{0};
-
     while (true) {
-        rd_res = recv(connected_socket_fd, buffer, sizeof(buffer), 0);
+        ssize_t rd_res = recv(connected_socket_fd, buffer, sizeof(buffer), 0);
 
         // Error occured
         if (rd_res == -1) {
