f21094d1f843f6dcfc02291873a72cf3690e6f4f
Maksim <maksimshchavelev@gmail.com>
2025-07-21 18:52:25 +0300

    fix inability to switch tabs with mods
    Reworked the way tabs are stored, now they are no longer newly created in `Renderer`
    which used to be passed to `m_screen.Loop`, but stored in a class. When necessary,
    their data is updated in the `set_data` method. Also fixed a major bug in `Tabs::OnRender()`
    which was that a reference to m_tabs[m_current_tab] was taken, but if m_tabs was empty
    it would result in a UB. The problem was discovered when building in Debug mode

--- client/include/modules/ui/internals/tabs.hpp
+++ client/include/modules/ui/internals/tabs.hpp
@@ -30,6 +30,14 @@ class Tabs : public ftxui::ComponentBase {
 
 
     /**
+     * @brief Default tabs constructor
+     */
+    Tabs() = default;
+
+
+
+
+    /**
      * @brief OnRender Draw the component. Build a ftxui::Element to be drawn on the ftxi::Screen
      * representing this ftxui::ComponentBase
      * @return `ftxui::Element`
@@ -45,6 +53,18 @@ class Tabs : public ftxui::ComponentBase {
      * @return `true` if event is catched, else `false`
      */
     bool OnEvent(ftxui::Event e) override;
+
+
+
+
+    /**
+     * @brief Sets new tabs and their contents
+     * @param headers Tab headers
+     * @param content Tab contents
+     * @throw Throws an exception if the number of headers is not equal to the number of tabs
+     * @note Use via `dynamic_cast`
+     */
+    void set_data(std::vector<ftxui::Element>&& headers, std::vector<ftxui::Component>&& content);
 
 
 
