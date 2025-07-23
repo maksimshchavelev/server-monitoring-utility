3a83f257c87ab012eb9bb765f88c91a14eee1d72
Maksim <maksimshchavelev@gmail.com>
2025-07-23 08:51:06 +0300

    refactor: module status check is now in Application instead of in the module
    This change will allow the application core to decide whether to collect data from the module.
    Previously, this was decided by the module

--- server/src/core/internals/app.cpp
+++ server/src/core/internals/app.cpp
@@ -68,6 +68,11 @@ Json::Value smu_server::Application::collect_metrics() {
 
     std::lock_guard<std::mutex> lock(m_modules_mutex);
     for (const auto& module : m_modules) {
+        // Skip module if module is disabled
+        if(!module->is_enabled()) {
+            continue;
+        }
+
         if (auto module_data = module->get_data(); module_data.has_value()) {
             root[module->module_name().data()] = std::move(module_data.value());
         }
