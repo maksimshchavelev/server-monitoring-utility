14cd8e90e36f73f42ebddde58c18a625791631b3
Maksim <maksimshchavelev@gmail.com>
2025-08-04 13:47:40 +0300

    docs: update field docs for CLI

--- server/include/cli/cli.hpp
+++ server/include/cli/cli.hpp
@@ -16,13 +16,18 @@ namespace smu_server {
 class Application; // Forward declaration to avoid cyclic including
 
 /**
- * @brief The CLI class for process CLI commands
+ * @brief The CLI class to processing CLI commands
+ *
+ * This class has full access to `Application` and its internal methods and fields. It
+ * autonomously processes incoming commands and returns a response. It uses the `IPC` class for
+ * interprocess communication with the `smu-cli` utility.
  */
 class CLI {
   public:
     /**
      * @brief CLI default construcor. CLI has access to all Application fields and methods
-     * @param app `Application` object
+     * @param app Reference to `Application`
+     * @note Command listening does not start automatically. Call `CLI::run` for this.
      */
     CLI(Application& app);
 
@@ -30,14 +35,14 @@ class CLI {
 
 
     /**
-     * @brief Runs CLI
+     * @brief Runs CLI async
      */
     void run();
 
 
   private:
-    Application&       m_app;
-    std::optional<IPC> m_ipc; // Optional for lazy initialization in `run_async`
+    Application&       m_app; ///< Reference to Applicaiton instance
+    std::optional<IPC> m_ipc; ///< Optional for lazy initialization in `run_async`
 
 
     /**
