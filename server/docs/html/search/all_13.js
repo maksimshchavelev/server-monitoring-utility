9088284a005322fbd57547c155faffaec607587d
Maksim <maksimshchavelev@gmail.com>
2025-08-04 18:19:27 +0300

    docs: update docs due to commit a3619de95422f384ea80aee2ceb2c8b67377e8e

--- server/docs/html/search/all_13.js
+++ server/docs/html/search/all_13.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['save_5fmodule_5fconfig_0',['save_module_config',['../classsmu__server_1_1Config__IO.html#a9452dd53ee1d0da4c2d5d1a63727c8b8',1,'smu_server::Config_IO']]],
+  ['save_5fmodule_5fconfig_0',['save_module_config',['../classsmu__server_1_1Config__IO.html#a782bdb408593b4c369306a65596ce214',1,'smu_server::Config_IO']]],
   ['save_5fserver_5fconfig_1',['save_server_config',['../classsmu__server_1_1Config__IO.html#a1b6787f583f86672ce210d79f72190c4',1,'smu_server::Config_IO']]],
   ['section_2',['Practical Section',['../md_for-developers_2modules_21__own__module.html#autotoc_md13',1,'']]],
   ['send_5feveryone_3',['send_everyone',['../classsmu__server_1_1MainWebsocketController.html#ab51c90ae6e3a045ccb8342be5d09e30d',1,'smu_server::MainWebsocketController::send_everyone()'],['../classsmu__server_1_1Network.html#aeaee190159a388a6b429ca98bd950fae',1,'smu_server::Network::send_everyone()']]],
