030cdd5a0e47c1dc3b3b75986bec8a947c5dfc63
Maksim <maksimshchavelev@gmail.com>
2025-08-18 10:09:41 +0300

    refactor: Network class now uses MDTP protocol

--- client/include/modules/network/network.hpp
+++ client/include/modules/network/network.hpp
@@ -31,12 +31,13 @@ class Network {
     /**
      * @brief Runs module
      * @param on_message Callback that is called when data is received from
-     * the server and has the signature `void (const std::string&)`
+     * the server and has the signature `void(const std::vector<uint8_t>&)`. Bytes are encoded in
+     * MDTP protocol
      * @param on_connection_error Callback that is called when a connection error occurs. The reason
      * message is passed to it
      */
-    void run(std::function<void(const std::string&)> on_message,
-             std::function<void(const std::string&)> on_connection_error);
+    void run(std::function<void(const std::vector<uint8_t>&)> on_message,
+             std::function<void(const std::string&)>          on_connection_error);
 
 
 
