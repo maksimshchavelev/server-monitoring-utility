6954ee31fe6f6d4916fd1ded0e33c6a7eeb00d8e
Maksim <maksimshchavelev@gmail.com>
2025-08-27 12:52:25 +0300

    refactor: tiny changes in ABI

--- server/include/abi/abi.hpp
+++ server/include/abi/abi.hpp
@@ -15,27 +15,27 @@ namespace smu_server {
 constexpr int ABI_VERSION = 1; ///< Version of ABI
 
 /**
- * @brief The context in which the module calls server core functions
- *
- * For example, for the logging function, you need to pass the context with the name of the module
- * that will be displayed in the logs.
- *
- * @note This is a packaged structure.
+ * @brief Struct to storing MDTP data. See documentation for MDTP protocol.
+ * @note This is a **packaged** structure.
  */
-struct __attribute__((packed)) ABI_CONTEXT {
-    const char* module_name;
-    const char* module_description;
-};
+typedef struct ABI_MODULE_MDTP_DATA {
+    const void* data; ///< Pointer to MDTP bytes
+    uint32_t    size; ///< Count of MDTP bytes (size of data)
+} ABI_MODULE_MDTP_DATA;
 
 
 /**
- * @brief Struct to storing MDTP data. See documentation for MDTP protocol
- * @note This is a packaged structure.
+ * @brief Context of module
+ *
+ * The context is needed so that the server core knows what module is accessing it (when calling
+ * core functions, for example, logging)
+ *
+ * @note This is a **packaged** structure
  */
-struct __attribute__((packed)) ABI_MDTP_DATA {
-    const uint8_t* data; ///< Pointer to data
-    const uint32_t size; ///< Size of data
-};
+typedef struct ABI_MODULE_CONTEXT {
+    const char* module_name;        ///< Name of module
+    const char* module_description; ///< Description of module
+} ABI_MODULE_CONTEXT;
 
 
 /**
@@ -45,17 +45,29 @@ struct __attribute__((packed)) ABI_MDTP_DATA {
  *
  * @note This is a packaged structure.
  */
-struct __attribute__((packed)) ABI_SERVER_CORE_FUNCTIONS {
-    void (*abi_log)(ABI_CONTEXT* context,
-                    int          log_type,
-                    const char*  message); ///< Logging function. Log types:
-                                          ///< `0`: White (info) log message
-                                          ///< `1`: Yellow (warning) log message
-                                          ///< '2': Red (error) log message
+typedef struct ABI_SERVER_CORE_FUNCTIONS {
+    /**
+     * @brief Get ABI version
+     * @param context Module context. See `ABI_MODULE_CONTEXT`
+     * @return ABI version
+     */
+    uint32_t (*abi_get_abi_version)(const ABI_MODULE_CONTEXT* context);
 
-    int (*abi_get_abi_version)(ABI_CONTEXT* context); ///< Returns ABI version
-};
+    /**
+     * @brief Logging function
+     *
+     * @param context Module context. See `ABI_MODULE_CONTEXT`
+     * @param log_type Type of log. Available log types:
+     *          - `0`: White (info) log message
+     *          - `1`: Yellow (warning) log message
+     *          - `2`: Red (error) log message
+     * @param message Message to log
+     */
+    void (*abi_log)(const ABI_MODULE_CONTEXT* context, int log_type, const char* message);
+} ABI_SERVER_CORE_FUNCTIONS;
 
+
+typedef struct ABI_MODULE_FUNCTIONS ABI_MODULE_FUNCTIONS; ///< Forward declaration
 
 /**
  * @brief Functions of the module that calls the server core
@@ -64,34 +76,31 @@ struct __attribute__((packed)) ABI_SERVER_CORE_FUNCTIONS {
  *
  * @note This is a packaged structure.
  */
-struct __attribute__((packed)) ABI_MODULE_FUNCTIONS {
+typedef struct ABI_MODULE_FUNCTIONS {
     ABI_MODULE_FUNCTIONS(*module_init)
     (ABI_SERVER_CORE_FUNCTIONS server_functions,
      const char*               json_configuration); ///< Initializes module
 
-    void (*module_destroy)(); ///< Destroys module
+    void (*module_destroy)(void); ///< Destroys module
 
-    const char* (*module_get_configuraion)(); ///< Get module json configuration
+    const char* (*module_get_configuration)(void); ///< Get module json configuration
 
-    ABI_MDTP_DATA (*module_get_data)(); ///< Get MDTP module data
+    const ABI_MODULE_MDTP_DATA* (*module_get_data)(void); ///< Get MDTP module data
 
-    void (*module_enable)(); ///< Enables a module
+    void (*module_enable)(void); ///< Enables a module
 
-    void (*module_disable)(); ///< Disables a module
+    void (*module_disable)(void); ///< Disables a module
 
-    bool (*module_is_enabled)(); ///< Is module enabled (true/false)
+    uint8_t (*module_is_enabled)(void); ///< Is module enabled (true/false)
 
-    void (*module_set_poll_ratio)(uint32_t poll_ratio); ///< Set poll ratio of module
+    const char* (*module_get_module_name)(void); ///< Get module name
 
-    uint32_t (*module_get_poll_ratio)(); ///< Get poll ratio of mobule
+    const char* (*module_get_module_description)(void); ///< Get module description
 
-    const char* (*module_get_module_name)(); ///< Get module name
+    void (*module_set_poll_ratio)(uint32_t poll_ratio); ///< Set the poll ratio of module
 
-    const char* (*module_get_module_description)(); ///< Get module description
-
-    ABI_CONTEXT* (*module_get_context)(); ///< Get context of module. See `ABI_CONTEXT`
-};
-
+    uint32_t (*module_get_poll_ratio)(void); ///< Get poll ratio of module
+} ABI_MODULE_FUNCTIONS;
 
 
 
@@ -101,12 +110,12 @@ struct __attribute__((packed)) ABI_MODULE_FUNCTIONS {
 /**
  * @brief C-callabe function for obtaining the ABI version
  */
-extern "C" int abi_get_abi_version(ABI_CONTEXT*);
+extern "C" uint32_t abi_get_abi_version(const ABI_MODULE_CONTEXT*);
 
 
 /**
  * @brief C-callabe function for logging
  */
-extern "C" void abi_log(ABI_CONTEXT* context, int log_type, const char* message);
+extern "C" void abi_log(const ABI_MODULE_CONTEXT* context, int log_type, const char* message);
 
 } // namespace smu_server
