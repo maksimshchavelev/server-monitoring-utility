7a2226b026bbc96bb5fcdffe2080069ec53203bc
Maksim <maksimshchavelev@gmail.com>
2025-11-19 16:01:12 +0300

    ram: the module now gets data from /proc/meminfo.

--- server/include/modules/ram/ram.hpp
+++ server/include/modules/ram/ram.hpp
@@ -8,7 +8,8 @@
 
 #pragma once
 
-#include "core/core.hpp"
+#include <core/core.hpp> // For server core utils
+#include <unordered_map> // For std::unordered_map
 
 namespace smu_server {
 
@@ -21,25 +22,39 @@ class RAM : public IModule {
   public:
     REGISTER_MODULE(RAM, "A module that allows you to get information about RAM")
 
-
-
-
     RAM(const Config& config);
-
-
-
 
     std::optional<std::vector<uint8_t>> get_data() override;
 
-
-
-
     void enable() override;
 
-
-
-
     void disable() override;
+
+  private:
+    /**
+     * @brief The function parses `/proc/meminfo` to obtain memory information. It returns data in
+     * the form of a hash table.
+     *
+     * For example, if you need to get the value of the `MemTotal` parameter, you should write code
+     * similar to the following:
+     *
+     * @code{.cpp}
+     * auto meminfo = parse_meminfo();
+     *
+     * // Make sure that the MemTotal key is actually present!
+     * std::cout << meminfo["MemTotal"];
+     * @endcode
+     *
+     * @param path Path to `meminfo`
+     *
+     * @throws Throws a `std::runtime_error` exception when an error occurs (with an error description)
+     *
+     * @return `std::unordered_map` with data. **All values are in bytes**
+     */
+    std::unordered_map<std::string /* parameter */, std::size_t /* value in bytes */> parse_meminfo(
+        const std::string_view path) const;
+
+    std::string m_meminfo_path; ///< Path to the meminfo file. /proc/meminfo by default
 };
 
 
--- server/src/modules/ram/ram.cpp
+++ server/src/modules/ram/ram.cpp
@@ -8,12 +8,9 @@
 
 
 #include "modules/ram/ram.hpp"
-#include <sys/sysinfo.h>
 
 
 namespace smu_server {
-
-
 
 
 // Public method
@@ -22,6 +19,7 @@ RAM::RAM(const Config& config) : IModule(config) {
     if (m_configuration.empty()) {
         m_configuration.set("enabled", true);
         m_configuration.set("poll_ratio", 1);
+        m_configuration.set("meminfo_path", "/proc/meminfo");
     }
 
     // Check if the `enabled` field exists. If not, log the error and throw an exception to abort
@@ -29,8 +27,7 @@ RAM::RAM(const Config& config) : IModule(config) {
     if (auto enabled = m_configuration.get<bool>("enabled"); enabled.has_value()) {
         m_enabled = enabled.value();
     } else {
-        log(LogType::ERROR, "The 'enabled' field is missing. Can't continue");
-        throw std::runtime_error("The 'enabled' field is missing");
+        throw std::runtime_error("The 'enabled' field in the configuration is missing");
     }
 
     // Check if the `poll_ratio` field exists. If not, log the error and throw an exception to abort
@@ -38,42 +35,59 @@ RAM::RAM(const Config& config) : IModule(config) {
     if (auto poll_ratio = m_configuration.get<unsigned int>("poll_ratio"); poll_ratio.has_value()) {
         m_poll_ratio = poll_ratio.value();
     } else {
-        log(LogType::ERROR, "The 'poll_ratio' field is missing. Can't continue");
-        throw std::runtime_error("The 'poll_ratio' field is missing");
+        throw std::runtime_error("The 'poll_ratio' field in the configuration is missing");
+    }
+
+    // Check if the `meminfo_path` field exists. If not, log the error and throw an exception to abort
+    // registration.
+    if (auto meminfo_path = m_configuration.get<std::string>("meminfo_path"); meminfo_path.has_value()) {
+        m_meminfo_path = meminfo_path.value();
+    } else {
+        throw std::runtime_error("The 'meminfo_path' field in the configuration is missing");
+    }
+
+    // Try to parse meminfo and print log
+    log(LogType::INFO, std::format("Trying to parse `{}`...", m_meminfo_path));
+    for (const auto& [key, value] : parse_meminfo(m_meminfo_path)) {
+        log(LogType::INFO, std::format("{}: {} kbytes", key, value / 1024));
     }
 }
 
 
-
-
 // Public method
 std::optional<std::vector<uint8_t>> RAM::get_data() {
-    struct sysinfo info;
+    std::unordered_map<std::string, std::size_t> meminfo;
 
-    if (sysinfo(&info) == -1) { // error
+    try {
+        meminfo = parse_meminfo(m_meminfo_path);
+    } catch (const std::runtime_error& e) {
+        log(LogType::ERROR, e.what());
         return std::nullopt;
-    } // else
+    }
+
+    // Ram info
+    const std::size_t totalram_mb = meminfo["MemTotal"] / 1024 / 1024;
+    const std::size_t usedram_mb = totalram_mb - meminfo["MemAvailable"] / 1024 / 1024;
+    const std::size_t usedram_percents = usedram_mb * 100 / totalram_mb;
+
+    // Swap info
+    const std::size_t totalswap_mb = meminfo["SwapTotal"] / 1024 / 1024;
+    const std::size_t usedswap_mb = totalswap_mb - meminfo["SwapFree"] / 1024 / 1024;
+    const std::size_t usedswap_percents = usedswap_mb * 100 / totalswap_mb;
 
 
-    auto root = make_root_node(
-        make_container_node(
-            "RAM info",
-            make_value_node("Total RAM", info.totalram / 1024 / 1024, "MB"),
-            make_value_node("Used RAM", (info.totalram - info.freeram) / 1024 / 1024, "MB"),
-            make_value_node(
-                "Used RAM (%)", (info.totalram - info.freeram) * 100 / info.totalram, "%")),
+    auto root = make_root_node(make_container_node("RAM info",
+                                                   make_value_node("Total RAM", totalram_mb, "MB"),
+                                                   make_value_node("Used RAM", usedram_mb, "MB"),
+                                                   make_value_node("Used RAM (%)", usedram_percents, "%")),
 
-        make_container_node(
-            "SWAP info",
-            make_value_node("Total SWAP", info.totalswap / 1024 / 1024, "MB"),
-            make_value_node("SWAP usage", (info.totalswap - info.freeswap) / 1024 / 1024, "MB"),
-            make_value_node(
-                "SWAP usage (%)", (info.totalswap - info.freeswap) * 100 / info.totalswap, "%")));
+                               make_container_node("SWAP info",
+                                                   make_value_node("Total SWAP", totalswap_mb, "MB"),
+                                                   make_value_node("SWAP usage", usedswap_mb, "MB"),
+                                                   make_value_node("SWAP usage (%)", usedswap_percents, "%")));
 
     return root->to_mdtp();
 }
-
-
 
 
 // Public method
@@ -83,12 +97,50 @@ void RAM::enable() {
 }
 
 
-
-
 // Public method
 void RAM::disable() {
     m_configuration.set("enabled", false);
     m_enabled = false;
+}
+
+
+// Private method
+std::unordered_map<std::string, std::size_t> RAM::parse_meminfo(const std::string_view path) const {
+    auto meminfo_read_result = SystemFileReader::read_text_file(path);
+
+    // Error occured
+    if (!meminfo_read_result.has_value()) {
+        // Allow the kernel to catch the exception itself and prevent the module from being
+        // registered or sending module data to the user.
+        throw std::runtime_error(std::format("error in method `parse_meminfo()`: `SystemFileReader::read_text_file` "
+                                             "returned error code {} (description: {}) for file `{}`",
+                                             static_cast<int>(meminfo_read_result.error()),
+                                             SystemFileReader::error_description(meminfo_read_result.error()),
+                                             path));
+    }
+
+    // Parse
+    std::unordered_map<std::string, std::size_t> result;
+
+    for (const auto& line : SystemFileReader::split_string(meminfo_read_result.value(), "\n")) {
+        const std::string_view key = SystemFileReader::token_before(line, ":");
+
+        // You need to parse the token after the colon and split it into the value and units of
+        // measurement.
+        const auto tokens_after_key =
+            SystemFileReader::split_string(SystemFileReader::trim(SystemFileReader::token_after(line, ":")));
+
+        // It's not the size, it's the quantity
+        if (tokens_after_key.size() <= 1)
+            continue;
+
+        std::size_t value = SystemFileReader::convert_units(
+            tokens_after_key[1], SystemFileReader::SizeUnit::BYTES, std::stoull(std::string(tokens_after_key[0])));
+
+        result[std::string(key)] = value;
+    }
+
+    return result;
 }
 
 } // namespace smu_server
